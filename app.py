68fa6eacebc2aba64cf454fb46984071499013de
Juan Rodriguez <satanasdelcolo@gmail.com>
2024-07-05 17:11:22 -0400

    Impresion pipelines y carga productos desde el front

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -10,8 +10,40 @@ coleccion = db['producto']
 
 @app.route('/listar')
 def listar_productos():
-    productos = list(coleccion.find())
+    pipeline = [
+        {
+            '$match': {}
+        },
+        {
+            '$project': {
+                'nom_prcto': 1,
+                'categoria': 1,
+                'precio': 1,
+                'caracteristicas': 1,
+                'stock': 1
+            }
+        }
+    ]
+    productos = list(coleccion.aggregate(pipeline))
     return render_template('listar.html', productos=productos)
+
+@app.route('/agregar', methods=['GET', 'POST'])
+def agregar_producto():
+    if request.method == 'POST':
+        nuevo_producto = {
+            'nom_prcto': request.form['nom_prcto'],
+            'categoria': request.form['categoria'],
+            'precio': float(request.form['precio']),
+            'caracteristicas': {
+                'lote': request.form['lote'],
+                'marca': request.form['marca'],
+                'fechaVencimiento': request.form['fechaVencimiento']
+            },
+            'stock': int(request.form['stock'])
+        }
+        coleccion.insert_one(nuevo_producto)
+        return redirect(url_for('listar_productos'))
+    return render_template('agregar.html')
 
 @app.route('/')
 def index():
@@ -19,4 +51,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
